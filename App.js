22169adb688d70a7281c5a4329cb03856b023e16
Joshua Haputantri <joshuahaputantri08@gmail.com>
2024-03-21 17:36:38 +0530

    Revisions to the App.js file to accommodate the changes done to the LoginScreen
    These are the lines for navigation path code for LoginPage. Modify these to fix the issues that we are facing.

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import LoginSignupScreen from './screens/LoginSignupScreen';
+import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import MovieDetailsScreen from './screens/MovieDetailsScreen';
 import BookmarksScreen from './screens/BookmarksScreen';
@@ -17,6 +18,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="LoginSignup">
         <Stack.Screen name="LoginSignup" component={LoginSignupScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="LoginPage" component={LoginScreen} options={{headerShown: false}} />
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'FlixTok' }} />
         <Stack.Screen name="MovieDetails" component={MovieDetailsScreen} />
         <Stack.Screen name="Bookmarks" component={BookmarksScreen} options={{ title: 'Bookmarks' }} />
