36442a353a0c11fab001a1e7982e34c5d03516fc
Joshua Haputantri <joshuahaputantri08@gmail.com>
2024-03-21 15:08:12 +0530

    Creating Bookmarks Screen
    Created the Bookmarks Page

---
+++ screens/BookmarksScreen.js
@@ -0,0 +1,16 @@
+// screens/BookmarksScreen.js
+
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+const BookmarksScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text>Bookmarks Screen</Text>
+      {/* Display bookmarked movies */}
+      <Button title="Go to Movie Details" onPress={() => navigation.navigate('MovieDetails')} />
+    </View>
+  );
+}
+
+export default BookmarksScreen;
