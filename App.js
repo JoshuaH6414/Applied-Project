465c697cae630faaaf9ecdced3d8f7362451b46a
Joshua Haputantri <joshuahaputantri08@gmail.com>
2024-03-25 16:50:43 +0530

    Added the bottom bar navigator to App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import LoginSignupScreen from './screens/LoginSignupScreen';
 import HomeScreen from './screens/HomeScreen';
@@ -10,8 +11,21 @@ import MovieDetailsScreen from './screens/MovieDetailsScreen';
 import BookmarksScreen from './screens/BookmarksScreen';
 import UserAccountScreen from './screens/UserAccountScreen';
 import SignUpScreen from './screens/SignUpScreen';
+import BottomTabBar from './components/BottomTabBar';
+
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Bookmarks" component={BookmarksScreen} />
+      <Tab.Screen name="UserAccount" component={UserAccountScreen} />
+    </Tab.Navigator>
+  );
+};
 
 const App = () => {
   return (
