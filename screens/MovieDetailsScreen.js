8be1b8119f6ef50c3533a471d46b09fcf1191dbe
Joshua Haputantri <joshuahaputantri08@gmail.com>
2024-04-27 20:40:31 +0530

    Update MovieDetailsScreen.js

--- screens/MovieDetailsScreen.js
+++ screens/MovieDetailsScreen.js
@@ -1,17 +1,101 @@
-// screens/MovieDetailsScreen.js
-
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 
-const MovieDetailsScreen = ({ navigation }) => {
+const MovieDetailsScreen = ({ navigation, route }) => {
+  // Extract movie details from the route params
+  const { movie } = route.params;
+
+  // Check if movie is null before accessing its properties
+  if (!movie) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.instructions}>Go Back and Swipe Left or Right on the card to begin!</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>Movie Details Screen</Text>
-      {/* Display movie details */}
-      <Button title="Go back" onPress={() => navigation.goBack()} />
+    <View style={styles.container}>
+      <Text style={styles.title}>Movie Details</Text>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.detailsBox}>
+          <Text style={styles.label}>Title:</Text>
+          <Text style={styles.text}>{movie.title}</Text>
+        </View>
+        <View style={styles.detailsBox}>
+          <Text style={styles.label}>Description:</Text>
+          <Text style={styles.text}>{movie.overview}</Text>
+        </View>
+        <View style={styles.detailsBox}>
+          <Text style={styles.label}>Genres:</Text>
+          <Text style={styles.text}>{movie.genres}</Text>
+        </View>
+        <View style={styles.detailsBox}>
+          <Text style={styles.label}>Release:</Text>
+          <Text style={styles.text}>{movie.releaseDate}</Text>
+        </View>
+      </ScrollView>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
+        <Text style={styles.buttonText}>Go back</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: 'black',
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginTop: 40,
+    marginBottom: 30,
+    color: "white",
+    textAlign: "center"  
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 23,
+    marginBottom: 10,
+    color: "white",
+  },
+  text: {
+    marginBottom: 20,
+    color: "white",
+    fontSize:16,
+  },
+  instructions: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: "white"
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: '#1E90FF',
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  detailsBox: {
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 20,
+  },
+  scrollView: {
+    flex: 1,
+    maxHeight: 400,
+    //maxWidth: 400,
+    
+  },
+});
+
 export default MovieDetailsScreen;
- 
