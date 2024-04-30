29163f209efd4c7a5797c6296ce5489866587b62
Joshua Haputantri <joshuahaputantri08@gmail.com>
2024-04-30 18:31:16 +0530

    Update MovieDetailsScreen.js
    Added the reviews to the Movie details page.
    Now displays a message if there are no reviews.
    changes to the UI and background colour.

--- screens/MovieDetailsScreen.js
+++ screens/MovieDetailsScreen.js
@@ -34,6 +34,21 @@ const MovieDetailsScreen = ({ navigation, route }) => {
           <Text style={styles.label}>Release:</Text>
           <Text style={styles.text}>{movie.releaseDate}</Text>
         </View>
+        {movie.reviews.length > 0 ? (
+          <View style={styles.detailsBox}>
+            <Text style={styles.label}>Reviews:</Text>
+            {movie.reviews.map(review => (
+              <View key={review.reviewDate}>
+                <Text style={styles.text}>-------------------------------------------</Text>
+                <Text style={styles.text}>Author: {review.author}</Text>
+                <Text style={styles.text}>Date: {review.reviewDate}</Text>
+                <Text style={styles.text}>Content: {review.content}</Text>
+              </View>
+            ))}
+          </View>
+        ) : (
+          <Text style={styles.text}>No reviews available for this movie.</Text>
+        )}
       </ScrollView>
       <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
         <Text style={styles.buttonText}>Go back</Text>
@@ -47,7 +62,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 20,
-    backgroundColor: 'black',
+    backgroundColor: '#0b0f54',
   },
   title: {
     fontSize: 35,
@@ -94,7 +109,6 @@ const styles = StyleSheet.create({
     flex: 1,
     maxHeight: 400,
     //maxWidth: 400,
-    
   },
 });
 
