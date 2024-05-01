f5b27596199e6a32cf3e071fc58a9fdbeba75e4e
Joshua Haputantri <joshuahaputantri08@gmail.com>
2024-05-01 11:30:56 +0530

    Update firebaseConfig.js
    USING NEW TEST DATABASE

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -23,13 +23,13 @@ import {getAuth, initializeAuth, getReactNativePersistence} from 'firebase/auth'
 
 // Test DB
 const firebaseConfig = {
-  apiKey: "AIzaSyCEwmseSNQ0pjQcRAzVmy_treppq6Hu6pA",
-  authDomain: "test-870cb.firebaseapp.com",
-  projectId: "test-870cb",
-  storageBucket: "test-870cb.appspot.com",
-  messagingSenderId: "998056003172",
-  appId: "1:998056003172:web:5badf25e5bb6b07e214171",
-  measurementId: "G-PGF4RNVGKJ"
+  apiKey: "AIzaSyA82iw1zmPZsw5sax2iXUlIq9EYRWtXCEU",
+  authDomain: "test-index-f1868.firebaseapp.com",
+  projectId: "test-index-f1868",
+  storageBucket: "test-index-f1868.appspot.com",
+  messagingSenderId: "15807652300",
+  appId: "1:15807652300:web:ff0cee5a6da2024f277d69",
+  measurementId: "G-C0B3KGSZFD"
 };
 
 // Initialize Firebase
