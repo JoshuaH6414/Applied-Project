739d5fbe7810aa8dcd0866e978c1f9bc7de15189
ShaneKavinda <shanekavinda1999@gmail.com>
2024-05-09 20:14:34 +0530

    changed styles for added bookmarks

--- components/BookmarksScreenComponent.js
+++ components/BookmarksScreenComponent.js
@@ -1,38 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
-import { collection, getDocs, query, limit } from 'firebase/firestore';
-import { db, auth } from '../firebaseConfig'; // Import both db and auth from your Firebase configuration
+import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebaseConfig';
+import { auth } from '../firebaseConfig';
 
 const BookmarksScreen = () => {
   const [bookmarkedMovies, setBookmarkedMovies] = useState([]);
+  const currentUser = auth.currentUser;
+  const userId = currentUser ? currentUser.uid : null;
 
   // Function to fetch bookmarked movies for the current user from the database
   const fetchBookmarkedMovies = async () => {
     try {
-      const currentUser = auth.currentUser;
-      const userId = currentUser ? currentUser.uid : null;
       if (userId) {
-        const userDocRef = collection(db, 'likedBookmarks', userId, 'bookmarks');
-        const bookmarksQuery = query(userDocRef, limit(10)); // Limit the query to fetch only 10 documents
-        const querySnapshot = await getDocs(bookmarksQuery);
-
-        if (!querySnapshot.empty) {
-          const bookmarkedMoviesData = [];
-          querySnapshot.forEach((doc) => {
-            bookmarkedMoviesData.push(doc.data());
-          });
-
-          setBookmarkedMovies(bookmarkedMoviesData);
-        } else {
-          console.log('No bookmarked movies found.');
-        }
-      } else {
-        console.log('User not authenticated.');
+        const userBookmarksRef = collection(db, 'likedBookmarks', userId, 'bookmarks');
+        const bookmarksQuerySnapshot = await getDocs(userBookmarksRef);
+  
+        const bookmarkedMoviesData = [];
+        bookmarksQuerySnapshot.forEach((doc) => {
+          const movieData = doc.data(); // Data for each movie
+          bookmarkedMoviesData.push(movieData);
+        });
+  
+        console.log('Bookmarked movies:', bookmarkedMoviesData);
+        setBookmarkedMovies(bookmarkedMoviesData);
       }
     } catch (error) {
       console.error('Error fetching bookmarked movies:', error);
     }
   };
+  
 
   useEffect(() => {
     fetchBookmarkedMovies();
@@ -40,10 +37,11 @@ const BookmarksScreen = () => {
 
   // Render function for each bookmarked movie item
   const renderMovieItem = ({ item }) => (
-    <View style={styles.movieItem}>
-      <Text style={styles.title}>{item.title}</Text>
+    <TouchableOpacity style={styles.movieItem}>
+      <Text style={styles.movieTitle}>{item.title}</Text>
+      <Text style={styles.releaseDate}>{item.releaseDate}</Text>
       {/* Add any additional movie details you want to display */}
-    </View>
+    </TouchableOpacity>
   );
 
   return (
@@ -54,6 +52,7 @@ const BookmarksScreen = () => {
           data={bookmarkedMovies}
           renderItem={renderMovieItem}
           keyExtractor={(item, index) => index.toString()}
+          contentContainerStyle={styles.flatListContainer}
         />
       ) : (
         <Text style={styles.emptyMessage}>No movies bookmarked yet.</Text>
@@ -68,21 +67,38 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#003426',
+    paddingTop: 40, // Add padding top to move content down
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     marginBottom: 10,
     color: 'white',
+    marginTop: 20, // Adjust margin top to move the text down
+  },
+  flatListContainer: {
+    alignItems: 'stretch',
+    paddingHorizontal: 20, // Add horizontal padding to the FlatList content
   },
   movieItem: {
+    backgroundColor: '#348833',
+    borderRadius: 10,
     padding: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
+    marginBottom: 15,
+  },
+  movieTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#DEFFDE'
+  },
+  releaseDate: {
+    fontSize: 14,
+    color: '#99BB99',
+    marginTop: 5,
   },
   emptyMessage: {
     fontSize: 18,
-    color: '#777',
+    color: '#777777',
   },
 });
 
